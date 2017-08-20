7170f73634343e6c60da25b3131b780d0e8cd025
spracto <barclayiversen@gmail.com>
2017-08-20 15:45:45 -0700

    Section 7 lecture 31

--- index.android.js
+++ index.android.js
@@ -3,11 +3,15 @@
 //import a library to help create Component
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
-
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 //create a Component
 const App = () => (
+  <View>
     <Header headerText='Albums' />
+    <AlbumList />
+  </View>
+
   );
 
 //Render it
