3ba3adbe5aa49eec43268022c6dfc23d6c0d3da1
Barclay Iversen <biversen@biversen-macbookpro.roam.corp.google.com>
2017-09-12 12:02:48 -0700

    Section 8 lecture 42

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,17 @@
 // iOS
+//import a library to help create Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+//create a Component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText='Albums' />
+    <AlbumList />
+  </View>
+
+  );
+
+//Render it
+AppRegistry.registerComponent('albums', () => App);
